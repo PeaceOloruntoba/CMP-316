35969f5a15ba9050227ca15e42344b5a5160b7b5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-11-22 23:33:47 +0100

    dfgbn

---
+++ first.py
@@ -0,0 +1,21 @@
+# numbers = [34, 54, 67, 21, 78, 97, 45, 44, 80, 19]
+# total =0
+# for num in numbers:
+#     if num%2 == 0:
+#         print(num)
+#         total = num + total
+
+# print("The total number is ", total)
+
+zen = '''
+beautiful is better than ugly
+explixit is better than implicit
+simple is better than complexcomplex is better than complicated
+'''
+
+for char in zen:
+    if char not in 'aeiou':
+        print(char, end='')
+
+NQKCENJL
+
