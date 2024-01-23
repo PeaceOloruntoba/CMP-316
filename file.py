9b40d24c1c52e439c4aa872ceda3e12791291254
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-01-23 10:10:15 +0100

    xcv

--- file.py
+++
@@ -1,7 +0,0 @@
-# with open("test.txt",'w',encoding = 'utf-8') as f:
-#   f.write("my first file\n")
-#   f.write("my first file\n")
-#   f.write("my first file\n")
-
-f = open("test.txt",'r',encoding='utf-8')
-f.read(4)
