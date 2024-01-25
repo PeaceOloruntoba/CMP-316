3918bfc355b100c0f259badbd7b07929e65ba43a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-01-25 12:50:32 +0100

    sdfgh

--- Prepare/first.py
+++ Prepare/first.py
@@ -1,5 +1,6 @@
-print("I am a baller!")
-name = input("What is your name? ")
-print("Hello " + name)
-number = int(input("Enter any number: "))
-print(number + 10)
+# print("I am a baller!")
+# name = input("What is your name? ")
+# print("Hello " + name)
+# number = int(input("Enter any number: "))
+# print(number + 10)a
+
