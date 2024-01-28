8168082e2b14a3ed0197f88aed4e4554412b8509
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-01-28 16:54:13 +0100

    sdfg

---
+++ ass4/upper to lower case.py
@@ -0,0 +1,6 @@
+text = "This is a String to Convert"
+uppercase_text = text.upper()
+lowercase_text = text.lower()
+print(f"Original text: {text}")
+print(f"Uppercase: {uppercase_text}")
+print(f"Lowercase: {lowercase_text}")
