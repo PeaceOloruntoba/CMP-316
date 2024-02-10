7739b0469514a2496852186bd37bca9fc4d559bf
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-10 09:34:42 +0100

    jhjh

---
+++ ass4/simple_calculator.py
@@ -0,0 +1,20 @@
+# Get user input for numbers and operation
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Cannot divide by zero"
+else:
+    result = "Invalid operation"
+
+print(f"The result of {num1} {operation} {num2} is: {result}")
